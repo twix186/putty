fcdd460498837871bdbb171f320831a28d54c84f
twix186 <48436310+twix186@users.noreply.github.com>
2019-12-18 17:53:58 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 2
+                    Laravel 123132
                 </div>
 
                 <div class="links">
